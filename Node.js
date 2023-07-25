22ce06353b958857c5782d4dc5372121e3d7cfe7
JesusRondon098 <rondonbautista1@gmail.com>
2023-07-25 15:26:26 +0200

    x

---
+++ Node.js
@@ -0,0 +1,22 @@
+//
+console.log("*****Reto2****");
+function sumar(x,y) {
+    let resultado = x + y
+    return resultado
+}
+function restar(x,y) {
+    let resultado = x - y
+    return resultado
+}
+function producto(x,y) {
+    let resultado = x * y
+    return resultado
+}
+function dividir(x,y) {
+    let resultado = x / y
+    return resultado
+}
+function cuadrado(x){
+    let resultado = Math.sqrt(x);
+    return resultado
+}
